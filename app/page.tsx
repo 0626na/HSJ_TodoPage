247e0e4a70f1199060b73510b9d8631fa0b90d7f
SUKJIN HAN <0626na@gmail.com>
2025-02-16 00:12:54 +0900

    Todo 앱의 메인 페이지 수정
    - 보드 제목을 입력하고 수정하는 기능과 input을 추가
    - 처음 로드시에 useEffect hook으로 localStorage의 데이터 여부를 파악하고 기존에 데이터가 있는지를 확인 및 데이터 로딩
    - 현재 가지고 있는 Todo Board 들을 페이지에 생성

--- app/page.tsx
+++ app/page.tsx
@@ -1,52 +1,54 @@
 "use client";
 
-import { useState } from "react";
+import BoardComponent from "@/app/components/Board";
+import { getInitialState, STORAGE_KEY, todoReducer } from "@/app/utils";
+import { useEffect, useReducer, useState } from "react";
 
 export default function Home() {
-  const [todoList, setTodoList] = useState<string[]>([]);
-  const [inputValue, setInputValue] = useState("");
+  const [boards, dispatch] = useReducer(
+    todoReducer,
+    undefined,
+    getInitialState
+  );
+
+  const [newBoardTitle, setNewBoardTitle] = useState("");
+
+  // 보드 추가 함수
+  const handleBoardKeyDown = (e: React.KeyboardEvent<HTMLInputElement>) => {
+    // Enter 키 눌렀을 때만 처리
+    if (e.nativeEvent.isComposing || e.key !== "Enter") return;
+
+    const title = newBoardTitle.trim();
+    if (!title) return;
+
+    dispatch({ title, type: "ADD_BOARD" });
+    setNewBoardTitle("");
+  };
+
+  // 상태가 변경될 때마다 localStorage에 저장
+  useEffect(() => {
+    localStorage.setItem(STORAGE_KEY, JSON.stringify(boards));
+  }, [boards]);
 
   return (
-    /**
-     * 초기, 기본적인 todo 추가 및 삭제 기능 구현한 상태.
-     * 보드 신규 생성 및 삭제 기능을 추가할 예정.
-     */
-    <div>
-      <h1>Todo를 입력해주세요</h1>
+    <div className="flex flex-col h-screen p-4 max-w-screen-2xl mx-auto">
+      <div className="flex-shrink-0">
+        <h1 className="text-2xl font-bold mb-4">보드 추가</h1>
+        <input
+          type="text"
+          placeholder="새 보드 제목을 입력하세요"
+          value={newBoardTitle}
+          onChange={(e) => setNewBoardTitle(e.currentTarget.value)}
+          onKeyDown={handleBoardKeyDown}
+          className="p-1 border border-gray-300 rounded w-full mb-4"
+        />
+      </div>
 
-      <input
-        value={inputValue}
-        onChange={(e) => setInputValue(e.currentTarget.value)}
-        type="text"
-        onKeyDown={(e) => {
-          /*
-           * e.nativeEvent.isComposing은 Composition 이벤트가 진행중인지를 확인하는 프로퍼티.
-           * Composition 이벤트가 react rendering과 별개로 이루어져서 무작위로 이상한 값이 들어가는 경우가 있기에
-           * Composition 이벤트가 진행중일때는 todo를 추가하지 않도록 설정
-           */
-          if (e.nativeEvent.isComposing || e.key != "Enter") return;
-
-          setTodoList((prev) => [inputValue, ...prev]);
-          setInputValue("");
-        }}
-        placeholder="할일을 입력하세요"
-      />
-
-      <h3>할일 목록</h3>
-      <ul>
-        {todoList.map((todo, index) => (
-          <li key={index}>
-            {todo}
-            <button
-              onClick={() =>
-                setTodoList(todoList.filter((_item, i) => i !== index))
-              }
-            >
-              삭제
-            </button>
-          </li>
+      <div className="overflow-y-auto w-full grid items-start gap-4 md:grid-cols-4 xl:grid-cols-6 mt-2">
+        {boards.map((board) => (
+          <BoardComponent key={board.id} board={board} dispatch={dispatch} />
         ))}
-      </ul>
+      </div>
     </div>
   );
 }
