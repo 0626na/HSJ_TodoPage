b433dc176db15c85e3495bc78a6e1c8aac8ecdd3
SUKJIN HAN <0626na@gmail.com>
2025-02-16 22:11:23 +0900

    최상위 page에 dnd-kit 라이브러리를 사용한 drag & drop 관련 기능 추가

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,27 @@
 "use client";
 
+import AddBoard from "@/app/components/AddBoard";
 import BoardComponent from "@/app/components/Board";
-import { getInitialState, STORAGE_KEY, todoReducer } from "@/app/utils";
+import BoardList from "@/app/components/BoardList";
+import Todo from "@/app/components/Todo";
+import { Board, DraggableData, Todo as TodoType } from "@/app/type";
+import {
+  findBoardById,
+  findTodoById,
+  getInitialState,
+  STORAGE_KEY,
+  todoReducer,
+} from "@/app/utils";
+import {
+  DndContext,
+  DragEndEvent,
+  DragOverlay,
+  DragStartEvent,
+  PointerSensor,
+  rectIntersection,
+  useSensor,
+  useSensors,
+} from "@dnd-kit/core";
 import { useEffect, useReducer, useState } from "react";
 
 export default function Home() {
@@ -11,18 +31,110 @@ export default function Home() {
     getInitialState
   );
 
-  const [newBoardTitle, setNewBoardTitle] = useState("");
+  // 현재 드래그 중인 Todo, Board
+  const [activeTodo, setActiveTodo] = useState<TodoType | null>(null);
+  const [activeBoard, setActiveBoard] = useState<Board | null>(null);
 
-  // 보드 추가 함수
-  const handleBoardKeyDown = (e: React.KeyboardEvent<HTMLInputElement>) => {
-    // Enter 키 눌렀을 때만 처리
-    if (e.nativeEvent.isComposing || e.key !== "Enter") return;
+  // dnd-kit 센서 설정
+  const sensors = useSensors(
+    useSensor(PointerSensor, { activationConstraint: { distance: 10 } })
+  );
 
-    const title = newBoardTitle.trim();
-    if (!title) return;
+  /**
+   * drag 시작할때 처리
+   */
+  const handleDragStart = (event: DragStartEvent) => {
+    const { active } = event;
+    if (active.data.current?.type === "todo") {
+      const todo = findTodoById(boards, active.id.toString());
+      setActiveTodo(todo);
+    } else if (active.data.current?.type === "board") {
+      // board인 경우
+      const board = findBoardById(boards, active.id.toString());
+      setActiveBoard(board);
+    }
+  };
 
-    dispatch({ title, type: "ADD_BOARD" });
-    setNewBoardTitle("");
+  // 보드 드래그 종료 처리 함수
+  const handleBoardDragEnd = (
+    active: DragEndEvent["active"],
+    over: DragEndEvent["over"]
+  ) => {
+    if (active.id !== over!.id) {
+      dispatch({
+        type: "REORDER_BOARDS",
+        activeId: active.id.toString(),
+        overId: over!.id.toString(),
+      });
+    }
+  };
+
+  // 투두 드래그 종료 처리 함수
+  const handleTodoDragEnd = (
+    active: DragEndEvent["active"],
+    over: DragEndEvent["over"]
+  ) => {
+    // active는 반드시 todo 타입임을 가정
+    const activeData = active.data.current as { type: "todo"; boardId: string };
+    const sourceBoardId = activeData.boardId;
+    let targetBoardId: string;
+    let overTodoId: string | null = null;
+
+    if (over && over.data.current) {
+      const overData = over.data.current as DraggableData;
+      if (overData.type === "todo") {
+        targetBoardId = (overData as { type: "todo"; boardId: string }).boardId;
+        overTodoId = over.id.toString();
+      } else if (overData.type === "todoContainer") {
+        targetBoardId = (overData as { type: "todoContainer"; boardId: string })
+          .boardId;
+      } else {
+        targetBoardId = over.id.toString();
+      }
+    } else {
+      return;
+    }
+
+    if (sourceBoardId === targetBoardId) {
+      if (overTodoId) {
+        dispatch({
+          type: "REORDER_TODOS",
+          boardId: sourceBoardId,
+          activeId: active.id.toString(),
+          overId: overTodoId,
+        });
+      }
+    } else {
+      dispatch({
+        type: "MOVE_TODO",
+        sourceBoardId,
+        targetBoardId,
+        todoId: active.id.toString(),
+        overId: overTodoId || "",
+      });
+    }
+  };
+
+  /**
+   * drag 끝났을때 처리
+   */
+  const handleDragEnd = (event: DragEndEvent) => {
+    const { active, over } = event;
+
+    // drag가 끝나면 현재 drag 중인 todo, board 해제
+    setActiveTodo(null);
+    setActiveBoard(null);
+
+    // 위치가 올바르지 않으면 종료
+    if (!over) return;
+    const activeData = active.data.current as DraggableData;
+    const overData = over.data.current as DraggableData;
+
+    if (activeData.type === "board" && overData.type === "board") {
+      handleBoardDragEnd(active, over);
+    } else if (activeData.type === "todo") {
+      handleTodoDragEnd(active, over);
+    }
   };
 
   // 상태가 변경될 때마다 localStorage에 저장
@@ -31,24 +143,34 @@ export default function Home() {
   }, [boards]);
 
   return (
-    <div className="flex flex-col h-screen p-4 max-w-screen-2xl mx-auto">
-      <div className="flex-shrink-0">
-        <h1 className="text-2xl font-bold mb-4">보드 추가</h1>
-        <input
-          type="text"
-          placeholder="새 보드 제목을 입력하세요"
-          value={newBoardTitle}
-          onChange={(e) => setNewBoardTitle(e.currentTarget.value)}
-          onKeyDown={handleBoardKeyDown}
-          className="p-1 border border-gray-300 rounded w-full mb-4"
-        />
-      </div>
+    <DndContext
+      sensors={sensors}
+      collisionDetection={rectIntersection}
+      onDragStart={handleDragStart}
+      onDragEnd={handleDragEnd}
+    >
+      {/* 드래그 할때의 Todo, Board 애니메이션 처리 */}
+      <DragOverlay>
+        {activeTodo ? (
+          <Todo
+            todo={activeTodo}
+            boardId="nothing"
+            onDelete={() => {}}
+            onUpdate={() => {}}
+            dispatch={() => {}}
+          />
+        ) : activeBoard ? (
+          <BoardComponent board={activeBoard} dispatch={() => {}} />
+        ) : null}
+      </DragOverlay>
 
-      <div className="overflow-y-auto w-full grid items-start gap-4 md:grid-cols-4 xl:grid-cols-6 mt-2">
-        {boards.map((board) => (
-          <BoardComponent key={board.id} board={board} dispatch={dispatch} />
-        ))}
+      <div className="flex flex-col w-full gap-6 items-center h-screen p-4 max-w-[2000px] mx-auto">
+        {/* Todolist 헤더 부분 */}
+        <AddBoard dispatch={dispatch} />
+
+        {/* board list */}
+        <BoardList boards={boards} dispatch={dispatch} />
       </div>
-    </div>
+    </DndContext>
   );
 }
