abfcacb0da05b14048bf245d573a7c483108ee7e
SUKJIN HAN <0626na@gmail.com>
2025-02-16 22:08:42 +0900

    Todo Drag & Drop 기능 추가 및 버튼 아이콘 추가

--- app/components/Todo.tsx
+++ app/components/Todo.tsx
@@ -1,13 +1,42 @@
 import { useState } from "react";
-import { Todo as TodoType } from "@/app/type";
+import { Action, Todo as TodoType } from "@/app/type";
+import { useSortable } from "@dnd-kit/sortable";
+import { CSS } from "@dnd-kit/utilities";
+import {
+  CheckIcon,
+  PencilSquareIcon,
+  TrashIcon,
+  XMarkIcon,
+} from "@heroicons/react/24/solid";
 
 interface TodoProps {
   todo: TodoType;
+  boardId: string;
   onDelete: () => void;
   onUpdate: (newText: string) => void;
+  dispatch: React.Dispatch<Action>;
 }
 
-const Todo = ({ todo, onDelete, onUpdate }: TodoProps) => {
+const Todo = ({ todo, onDelete, onUpdate, boardId }: TodoProps) => {
+  const {
+    attributes,
+    listeners,
+    setNodeRef,
+    transform,
+    transition,
+    isDragging,
+  } = useSortable({
+    id: todo.id,
+    data: { type: "todo", boardId },
+    animateLayoutChanges: () => false,
+  });
+
+  const style = {
+    transform: CSS.Transform.toString(transform),
+    transition: transition || "transform 100ms ease",
+    opacity: isDragging ? 0 : 1,
+  };
+
   // 수정 모드 여부를 관리하는 state
   const [isEditing, setIsEditing] = useState(false);
   // 수정 중인 값 (초기값은 현재 todo.text)
@@ -34,9 +63,15 @@ const Todo = ({ todo, onDelete, onUpdate }: TodoProps) => {
   };
 
   return (
-    <li className="flex justify-between items-center h-10 mb-1">
+    <li
+      ref={setNodeRef}
+      style={style}
+      {...attributes}
+      {...listeners}
+      className="flex justify-between items-center h-10 max-w-[300px]"
+    >
       {isEditing ? (
-        <div className="flex items-center w-full min-w-0">
+        <div className="flex items-center w-full overflow-x-auto">
           {/* 수정 모드일 때: 입력창과 확인/취소 버튼 */}
           <input
             type="text"
@@ -46,37 +81,38 @@ const Todo = ({ todo, onDelete, onUpdate }: TodoProps) => {
               if (e.nativeEvent.isComposing || e.key !== "Enter") return;
               handleConfirmClick();
             }}
-            className="w-full p-1 border border-gray-200 rounded h-full box-border"
+            className="w-full p-1 border-b border-gray-200 rounded box-border bg-transparent text-foreground focus:outline-none focus:ring-0"
           />
-          <div className="flex space-x-2 ml-2 flex-shrink-03">
-            <button
+          <div className="flex space-x-2 ml-2 flex-shrink-0">
+            {/* 수정 완료 */}
+            <CheckIcon
               onClick={handleConfirmClick}
-              className="text-blue-500 text-sm"
-            >
-              확인
-            </button>
-            <button
+              className="text-blue-500 size-4"
+            />
+
+            {/* 수정 취소 */}
+            <XMarkIcon
               onClick={handleCancelClick}
-              className="text-gray-500 text-sm"
-            >
-              취소
-            </button>
+              className="text-red-500 size-4"
+            />
           </div>
         </div>
       ) : (
-        <>
+        <div className="flex justify-between items-center w-full">
           {/* 일반 모드일 때: todo 텍스트와 수정/삭제 버튼 */}
           <span className="whitespace-nowrap overflow-x-auto">{todo.text}</span>
 
-          <div className="flex items-end gap-2 flex-shrink-0 pl-3">
-            <button onClick={handleEditClick} className="text-blue-500 text-sm">
-              수정
-            </button>
-            <button onClick={onDelete} className="text-red-500 text-sm">
-              삭제
-            </button>
+          <div className="flex items-center gap-3 flex-shrink-0 pl-3">
+            {/* 수정 */}
+            <PencilSquareIcon
+              className="size-4 text-blue-500"
+              onClick={handleEditClick}
+            />
+
+            {/* 삭제 */}
+            <TrashIcon onClick={onDelete} className="text-red-500 size-4" />
           </div>
-        </>
+        </div>
       )}
     </li>
   );
